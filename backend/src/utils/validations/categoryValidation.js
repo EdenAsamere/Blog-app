05a4568a4aa5a100a805a4b774913431e8c1851d
EdenAsamere <edenasamere77@gmail.com>
2024-04-11 00:24:24 -0700

    some changes

--- backend/src/utils/validations/categoryValidation.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createCategorySchema = void 0;
-var zod_1 = require("zod");
-exports.createCategorySchema = (0, zod_1.object)({
-    body: (0, zod_1.object)({
-        name: (0, zod_1.string)({ required_error: "category name is required" }).min(2, { message: 'user name should be minimum of 2 characters' })
-    })
-});
--- backend/src/utils/validations/commentValidation.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreateCommentSchema = void 0;
-var zod_1 = require("zod");
-exports.CreateCommentSchema = (0, zod_1.object)({
-    userId: (0, zod_1.string)(),
-    comment: (0, zod_1.string)().min(2).max(255),
-    postId: (0, zod_1.string)()
-});
