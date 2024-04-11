736cbf323fd3a8d5b0f675c09f02b5407ac961a3
EdenAsamere <edenasamere77@gmail.com>
2024-04-11 03:20:48 -0700

    little modifications

--- backend/app.ts
+++ backend/app.ts
@@ -1,5 +1,5 @@
-import *as express from 'express';
-import * as cors from 'cors';
+import express from 'express';
+import cors from 'cors';
 import connectDB from './src/config/connectDB';
 import post from './src/routes/postRoute';
 import comment from './src/routes/commentRoute';
